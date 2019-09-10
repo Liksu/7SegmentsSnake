a5f086fd462aeac1dec03921d90451cda7f93406
Petro Borshchahivskyi <dev@Liksu.com>
2019-09-10 18:30:48 +0300

    Add some comments to sample script

--- examples/SnakeBasic/SnakeBasic.ino
+++ examples/SnakeBasic/SnakeBasic.ino
@@ -1,18 +1,24 @@
 #include <LedControl.h>
 #include "Snake.h"
 
-LedControl ledControl = LedControl(11, 13, 10, 1);
+// connect display with LedControl
+LedControl ledControl = LedControl(12, 11, 10, 1);
 
+// create Snake instance
+// parameters: ledControl (required), digitsCount = 4, delay = 400 ms
 Snake snake(ledControl);
 
 void setup() {
+    // prepare display
     ledControl.shutdown(0, false);
     ledControl.setIntensity(0, 15);
     ledControl.clearDisplay(0);
 
+    // start snake
     snake.start();
 }
 
 void loop() {
+    // move snake
     snake.tick();
 }
